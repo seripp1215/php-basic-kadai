f125b4893054813e1783473df99d09351b79bbab
seripp1215 <seripp18@gmail.com>
2025-03-04 23:28:43 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<!DOCTYPE>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $total_score = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+    
+    echo $average_score = $total_score / 10
+    ?>
+  </p>
+</body>
+  
+</html>
